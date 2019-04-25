366767949779a1f6ac37ac21078e383264129366
Stein Jakob Nordbø <steinjak@gmail.com>
2019-04-25 09:37:02 +0200

    Fixed wrong path

--- scripts/update-pluginxml.sh
+++ scripts/update-pluginxml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use inline dependencies instead of relying on npm install
-sed -i 'node_modules/deps/g' plugin.xml
+sed -i 's#node_modules/cordova-sqlite-storage-dependencies#deps#g' plugin.xml
 # Remove pre-install hook not supported by PhoneGap Build
 sed -i '/hook type="before_plugin_install"/d' plugin.xml
 
